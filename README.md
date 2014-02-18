fd87ae84d6d51232ad286065308f354c1f2285f4
Albert <lb13810398408@gmail.com>
2014-02-18 11:13:43 +0800

    init

--- README.md
+++ README.md
@@ -1,4 +1,76 @@
 wechatface
-==========
+=====
 
-微信内置表情（不包含emoji部分）解析+替换图片。
+This library allows the handling and conversion of Wechat Face in `Javascript`.
+
+## Usage
+
+### Browser
+
+```html
+<link href="./wechatface.css" rel="stylesheet" type="text/css" />
+<script src="./jquery.js"></script>
+<script src="./wechatface.js"></script>
+```
+
+```js
+$('.message').html(wechatFace.faceToHTML(MSG));
+```
+
+#### Seajs
+
+```js
+seajs.config({
+  alias: {
+    wechatface: 'https://raw.github.com/leeeboo/wechatface/master/wechatface.js'
+  }
+});
+
+seajs.use(['wechatface'], function (wechatface) {
+  // TODO
+});
+// or
+define('test', function (require, exports, modules) {
+  var wechatface = require('wechatface');
+});
+```
+
+#### RequireJS (AMD)
+
+```js
+require.config({
+  paths: {
+    wechatface: 'https://raw.github.com/leeeboo/wechatface/master/wechatface.js'
+  }
+});
+
+require(['wechatface'], function (wechatface) {
+  // TODO
+});
+```
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2012 - 2014 Albert <lb13810398408@gmail.com>.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ wechatface.js
@@ -0,0 +1,148 @@
+;(function (name, definition) {
+  var hasDefine = typeof define === 'function';
+  var hasExports = typeof module !== 'undefined' && module.exports;
+
+  if (hasDefine) {
+    define(definition);
+  } else if (hasExports) {
+    module.exports = definition();
+  } else {
+    this[name] = definition();
+  }
+})('wechatFace', function () {
+
+var wechatFace = {};
+
+var WECHAT_FACE_MAP = jEmoji.WECHAT_FACE_MAP = {
+    '/::)' : '1',
+    '/::~' : '2',
+    '/::B' : '3',
+    '/::|' : '4',
+    '/:8-)' : '5',
+    '/::<' : '6',
+    '/::$' : '7',
+    '/::X' : '8',
+    '/::Z' : '9',
+    '/::\'(' : '10',
+    '/::-|' : '11',
+    '/::@' : '12',
+    '/::P' : '13',
+    '/::D' : '14',
+    '/::O' : '15',
+    '/::(' : '16',
+    '/::+' : '17',
+    '/:--b' : '18',
+    '/::Q' : '19',
+    '/::T' : '20',
+    '/:,@P' : '21',
+    '/:,@-D' : '22',
+    '/::d' : '23',
+    '/:,@o' : '24',
+    '/::g' : '25',
+    '/:|-)' : '26',
+    '/::!' : '27',
+    '/::L' : '28',
+    '/::>' : '29',
+    '/::,@' : '30',
+    '/:,@f' : '31',
+    '/::-S' : '32',
+    '/:?' : '33',
+    '/:,@x' : '34',
+    '/:,@@' : '35',
+    '/::8' : '36',
+    '/:,@!' : '37',
+    '/:!!!' : '38',
+    '/:xx' : '39',
+    '/:bye' : '40',
+    '/:wipe' : '41',
+    '/:dig' : '42',
+    '/:handclap' : '43',
+    '/:&-(' : '44',
+    '/:B-)' : '45',
+    '/:<@' : '46',
+    '/:@>' : '47',
+    '/::-O' : '48',
+    '/:>-|' : '49',
+    '/:P-(' : '50',
+    '/::\'|' : '51',
+    '/:X-)' : '52',
+    '/::*' : '53',
+    '/:@x' : '54',
+    '/:8*' : '55',
+    '/:pd' : '56',
+    '/:<W>' : '57',
+    '/:beer' : '58',
+    '/:basketb' : '59',
+    '/:oo' : '60',
+    '/:coffee' : '61',
+    '/:eat' : '62',
+    '/:pig' : '63',
+    '/:rose' : '64',
+    '/:fade' : '65',
+    '/:showlove' : '66',
+    '/:heart' : '67',
+    '/:break' : '68',
+    '/:cake' : '69',
+    '/:li' : '70',
+    '/:bome' : '71',
+    '/:kn' : '72',
+    '/:footb' : '73',
+    '/:ladybug' : '74',
+    '/:shit' : '75',
+    '/:moon' : '76',
+    '/:sun' : '77',
+    '/:gift' : '78',
+    '/:hug' : '79',
+    '/:strong' : '80',
+    '/:weak' : '81',
+    '/:share' : '82',
+    '/:v' : '83',
+    '/:@)' : '84',
+    '/:jj' : '85',
+    '/:@@' : '86',
+    '/:bad' : '87',
+    '/:lvu' : '88',
+    '/:no' : '89',
+    '/:ok' : '90',
+    '/:love' : '91',
+    '/:<L>' : '92',
+    '/:jump' : '93',
+    '/:shake' : '94',
+    '/:<O>' : '95',
+    '/:circle' : '96',
+    '/:kotow' : '97',
+    '/:turn' : '98',
+    '/:skip' : '99',
+    '/:oY' : '100'
+};
+
+function _createRegexp(map) {
+    var keys = Object.keys(map);
+    console.log(keys.join('|'));
+    return new RegExp(keys.join('|'), 'g');
+}
+
+var WECHAT_FACE_RE = null;
+/**
+ * Convert unified code to HTML.
+ * 
+ * @param {String} text
+ * @return {String} html with emoji classname.
+*/
+function faceToHTML(text) {
+    for (var face in WECHAT_FACE_MAP) {
+        text = text.replace(face, '<span class="wechatface wechatface_' + WECHAT_FACE_MAP[face] + '"></span>');
+    };
+    return text;
+    /*
+    return text.replace(WECHAT_FACE_RE, function (_, m) {
+        var em = WECHAT_FACE_MAP[m];
+        return '[表情：' + em + ']';
+    });
+    */
+}
+wechatFace.faceToHTML = faceToHTML;
+
+return wechatFace;
+
+});
